f252d0148eeeeb4d21b4c8a4dbeb95401138d368
Homam Hosseini <bluesnowball@gmail.com>
2014-01-19 15:43:07 +0800

    Powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+New-Alias -Name notepad 'C:\Program Files (x86)\Notepad++\notepad++.exe'
+New-Alias -Name sublime 'C:\Program Files (x86)\Sublime Text 3\sublime_text.exe'
+New-Alias -Name python 'C:\Python27\python.exe'
+New-Alias which get-command
